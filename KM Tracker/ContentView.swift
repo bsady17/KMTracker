27acc7ff42cb1a2549fa56f2f874241a41c0cecf
Brandon <brandon.sady@agilebits.com>
2024-02-27 22:56:36 -0700

    Initial Commit

---
+++ KM Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KM Tracker
+//
+//  Created by Brandon Sady on 2024-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KM Tracker/KM_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KM_TrackerApp.swift
+//  KM Tracker
+//
+//  Created by Brandon Sady on 2024-02-27.
+//
+
+import SwiftUI
+
+@main
+struct KM_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
